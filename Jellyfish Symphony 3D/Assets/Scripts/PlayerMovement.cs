7ce269a9adbb981b918a30f208feb798b28a547d
NordinWG <115358788+NordinWG@users.noreply.github.com>
2025-03-03 11:00:18 +0100

    project made

---
+++ Jellyfish Symphony 3D/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,122 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	[Header("Base setup")]
+	public int speed = 4;
+	public int sprintSpeed = 25;
+	public float jumpSpeed = 8.0f;
+	public float gravity = 20.0f;
+	public float mouseSensitivity = 1500f;
+	public float controllerSensitivity = 1500f;
+	private int currentSpeed;
+	private float rotationX = 0f;
+	private float rotationY = 0f;
+	private Vector3 moveDirection = Vector3.zero;
+	public float hor;
+	public float ver;
+	public Camera playerCamera;
+	private CharacterController characterController;
+
+	[Header("Canvas")]
+
+
+	[SerializeField]
+	private float cameraYOffset = 0.4f;
+	public bool canMove;
+
+	void Start()
+	{
+
+		speed = 4;
+		sprintSpeed = 25;
+
+
+		if (_avatar == null) return;
+
+		if (!_avatar.IsMe) return;
+
+		characterController = GetComponent<CharacterController>();
+		
+		// Assign the main camera if it's not assigned
+		if (playerCamera == null)
+		{
+			playerCamera = Camera.main;
+
+			if (playerCamera == null) return;
+		}
+
+		// Set the camera's parent to the player object, if not already done in the Inspector
+		if (playerCamera.transform.parent != transform)
+		{
+			playerCamera.transform.SetParent(transform);
+			playerCamera.transform.localPosition = new Vector3(0, cameraYOffset, 0); // Adjust if necessary
+		}
+
+		currentSpeed = speed;
+	}
+
+	void Update()
+	{
+
+		if (playerCamera != null && !playerCamera.gameObject.activeInHierarchy)
+		{
+			playerCamera.gameObject.SetActive(true);
+		}
+
+        canMove = true;
+        
+		Movement();
+		RotatePlayer();
+	}
+
+	void Movement()
+	{
+		if (!canMove) return;
+
+		hor = Input.GetAxis("Horizontal");
+		ver = Input.GetAxis("Vertical");
+
+		currentSpeed = Input.GetKey(KeyCode.LeftControl) ? sprintSpeed : speed;
+
+		Vector3 forward = transform.TransformDirection(Vector3.forward);
+		Vector3 right = transform.TransformDirection(Vector3.right);
+		float curSpeedX = currentSpeed * ver;
+		float curSpeedY = currentSpeed * hor;
+		float movementDirectionY = moveDirection.y;
+
+		moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+		if (Input.GetButton("Jump") && characterController.isGrounded)
+			moveDirection.y = jumpSpeed;
+		else
+			moveDirection.y = movementDirectionY;
+
+		if (!characterController.isGrounded)
+			moveDirection.y -= gravity * Time.deltaTime;
+
+		characterController.Move(moveDirection * Time.deltaTime);
+	}
+
+	void RotatePlayer()
+	{
+		if (!canMove) return;
+
+		float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+		float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+		float controllerX = Input.GetAxis("Controller X") * controllerSensitivity * Time.deltaTime;
+		float controllerY = Input.GetAxis("Controller Y") * controllerSensitivity * Time.deltaTime;
+		
+		float inputX = Mathf.Abs(mouseX) > Mathf.Epsilon ? mouseX : controllerX;
+		float inputY = Mathf.Abs(mouseY) > Mathf.Epsilon ? mouseY : controllerY;
+
+		rotationX = Mathf.Clamp(rotationX - inputY, -90f, 90f);
+		rotationY += inputX;
+
+		playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+		transform.localRotation = Quaternion.Euler(0, rotationY, 0);
+		
+		Cursor.visible = false;
+	}
+}
